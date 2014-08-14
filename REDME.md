79bcc67a984a7a120e54b8ab000f8ee17c06096c
Konrad Kuźnicki <konrad@kuznicki.me>
2014-08-15 00:00:32 +0100

    Initial commit that steals titles from tpb

---
+++ REDME.md
@@ -0,0 +1,10 @@
+Extreamly Simple Python Movie Auto Forecasting
+==============================================
+
+(it should tel what movies are trending [or at least which are popular])
+
+
+
+please run
+
+`python3 ./pirateripper.py`
---
+++ pirateripper.py
@@ -0,0 +1,59 @@
+import urllib.request, string, re
+
+def getTrs(data):
+  results = data.index('searchResult')
+  start = data.index('/thead', results) + 7
+  end = data.index('</table>', results)
+  return data[start:end]
+
+
+def getTitle(tr):
+  return re.findall('detLink.*?>(.*?)</a>', tr)[0]
+
+def filterWhiteSpace(name):
+  return re.sub(r'[\W_]+', ' ',name).strip()
+
+def getTitleAndYear(tr):
+  name = getTitle(tr)
+  m = re.findall('^(.*?)(19\d{2}|20\d{2})', name)
+  if len(m) > 0:
+    return [filterWhiteSpace(m[0][0]), m[0][1]]
+  else:
+    return [filterWhiteSpace(name), '']
+
+
+
+def getFromPage(page):
+  f = urllib.request.urlopen(page)
+  data = f.read().decode('utf-8')
+  trs = getTrs(data).split('</tr>\n	<tr>')
+
+  return [getTitleAndYear(tr) for tr in trs]
+
+
+
+def getTorrents(site, category):
+  buf = []
+  for i in range(100):
+    buf += getFromPage(site + category + '/' + str(i) + '/3/')
+  return buf
+
+
+def transposeMatrix(matrix):
+  return list(map(list, zip(*matrix)))
+
+movies = getTorrents('http://pirateproxy.in/browse/', '207')
+
+titles = transposeMatrix(movies)[0]
+
+years = transposeMatrix(movies)[1]
+
+print(len(titles))
+
+uniqTitles = set(titles)
+
+# print(uniqTitles)
+
+print(len(uniqTitles))
+
+print(sorted(set(years)))
